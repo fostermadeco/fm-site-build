73f55eee1b79ecd761835ff7ca97b60bf9283c50
Kelly Packer <kelly@kellypacker.com>
2016-11-10 15:15:50 -0700

    copying over some changes from old repo

--- samples/assets/js/main.js
+++ samples/assets/js/main.js
@@ -4,7 +4,7 @@
 // OPEN .PDF'S AND EXTERNAL LINKS IN A NEW WINDOW
 function buildExternal() {
 	$('a[href^="http:"], a[href^="https:"]').not('[href*="'+document.domain+'"]').attr('target','_blank');
-	$('a[href$=".pdf"]').attr('target', '_blank');
+	$('a[href$=".pdf"]').attr('target', '_blank').attr('rel', 'noopener noreferrer');
 }
 
 $(function(){
